019e3468774974b75688ebb483adecab85fd0452
RohanDoshi21 <rohan.doshi02@gmail.com>
2021-06-06 15:27:22 +0530

    Joined the UI
    joined ui made by dinesh and the main project
    
    Dinesh is working on unit 3 UI

---
+++ Rohan/06-06-2021/lib/units/unit1/HardnessCalculator.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+class HardnessCalculator extends StatefulWidget {
+  @override
+  _HardnessCalculatorState createState() => _HardnessCalculatorState();
+}
+
+class _HardnessCalculatorState extends State<HardnessCalculator> {
+  TextEditingController mghco32 = TextEditingController();
+  TextEditingController cahco32 = TextEditingController();
+  TextEditingController mgcl2 = TextEditingController();
+  TextEditingController caso4 = TextEditingController();
+  TextEditingController mgno32 = TextEditingController();
+  TextEditingController mgso4 = TextEditingController();
+
+  String tempoutput = "";
+  String permoutput = "";
+  String totoutput = "";
+  var mgcho32Value = 0.0;
+  var cahco32Value = 0.0;
+  var temporaryHardness = 0.0;
+  var mgcl2Value = 0.0;
+  var caso4Value = 0.0;
+  var mgno32Value = 0.0;
+  var mgso4Value = 0.0;
+  var permanentHardness = 0.0;
+  var totalHardness = 0.0;
+
+  // ignore: non_constant_identifier_names
+  void TotalHardness() {
+    mgcho32Value = double.parse(mghco32.text);
+    mgcho32Value = mgcho32Value * 100 / 146;
+    cahco32Value = double.parse(cahco32.text);
+    cahco32Value = cahco32Value * 100 / 162;
+    temporaryHardness = mgcho32Value + cahco32Value;
+    mgcl2Value = double.parse(mgcl2.text);
+    mgcl2Value = mgcl2Value * 100 / 95;
+    mgno32Value = double.parse(mgno32.text);
+    mgno32Value = mgno32Value * 100 / 148;
+    mgso4Value = double.parse(mgso4.text);
+    mgso4Value = mgso4Value * 100 / 120;
+    caso4Value = double.parse(caso4.text);
+    caso4Value = caso4Value * 100 / 136;
+    permanentHardness = mgcl2Value + mgno32Value + mgso4Value + caso4Value;
+    totalHardness = permanentHardness + temporaryHardness;
+    setState(() {
+      tempoutput =
+          "The temporary hardness is " + temporaryHardness.toStringAsFixed(4);
+      permoutput =
+          "The permanent hardness is " + permanentHardness.toStringAsFixed(4);
+      totoutput =
+          "The total hardness is " + totalHardness.toStringAsFixed(4);
+    });
+  }
+
+  // void PermanentHardness() {
+  //   mgcl2Value = double.parse(mgcl2.text);
+  //   mgcl2Value = mgcl2Value * 100 / 95;
+  //   mgno32Value = double.parse(mgno32.text);
+  //   mgno32Value = mgno32Value * 100 / 148;
+  //   mgso4Value = double.parse(mgso4.text);
+  //   mgso4Value = mgso4Value * 100 / 120;
+  //   caso4Value = double.parse(caso4.text);
+  //   caso4Value = caso4Value * 100 / 136;
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hardness Calculator'),
+      ),
+      body: Column(
+        children: [
+          Text('Enter the values'),
+          Text('Mg(HCO3)2'),
+          TextField(
+            keyboardType: TextInputType.number,
+            controller: mghco32,
+          ),
+          Text('Ca(HCO3)2'),
+          TextField(
+            keyboardType: TextInputType.number,
+            controller: cahco32,
+          ),
+          Text('MgCl2'),
+          TextField(
+            keyboardType: TextInputType.number,
+            controller: mgcl2,
+          ),
+          Text('MgSO4'),
+          TextField(
+            keyboardType: TextInputType.number,
+            controller: mgso4,
+          ),
+          Text('Mg(NO3)2'),
+          TextField(
+            keyboardType: TextInputType.number,
+            controller: mgno32,
+          ),
+          Text('CaSO4'),
+          TextField(
+            keyboardType: TextInputType.number,
+            controller: caso4,
+          ),
+          ElevatedButton(
+            child: Text(
+              'Submit',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            onPressed: () {
+              TotalHardness();
+            },
+          ),
+          Text(
+            tempoutput,
+          ),
+           Text(
+            permoutput,
+          ),
+           Text(
+            totoutput,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ Rohan/06-06-2021/lib/units/unit1/studymaterial.dart
@@ -0,0 +1,56 @@
+//Code Written by Rohan Doshi
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
+class StudyMaterialUnit1 extends StatefulWidget {
+  @override
+  _StudyMaterialUnit1State createState() => _StudyMaterialUnit1State();
+}
+
+class _StudyMaterialUnit1State extends State<StudyMaterialUnit1> {
+  PdfViewerController _pdfViewerController = new PdfViewerController();
+  
+  @override
+  void initState() {
+    _pdfViewerController = PdfViewerController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Unit 1 Study Material"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.keyboard_arrow_up,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              _pdfViewerController.previousPage();
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.keyboard_arrow_down,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              _pdfViewerController.nextPage();
+            },
+          )
+        ],
+      ),
+      body: Container(
+        child: SfPdfViewer.asset(
+          'lib/units/unit1/Files/StudyMaterialUnit1.pdf',
+          controller: _pdfViewerController,
+          onDocumentLoadFailed: (PdfDocumentLoadFailedDetails details) {
+            print("FAIL");
+          },
+        ),
+      ),
+    );
+  }
+}
