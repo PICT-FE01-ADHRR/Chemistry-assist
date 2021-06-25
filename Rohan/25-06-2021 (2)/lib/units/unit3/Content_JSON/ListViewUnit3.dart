b97507e6fe10283cc65437a3889f5c1b335fa122
RohanDoshi21 <rohan.doshi02@gmail.com>
2021-06-25 18:16:08 +0530

    Made sign-in system

---
+++ Rohan/25-06-2021 (2)/lib/units/unit3/Content_JSON/ListViewUnit3.dart
@@ -0,0 +1,22 @@
+import '../Quiz/quiz.dart';
+
+class ListViewUnit3 {
+  int id;
+  String title;
+
+  ListViewUnit3({required this.id, required this.title});
+}
+
+List content = [
+  ListViewUnit3(id: 1, title: "Study Material"),
+  ListViewUnit3(id: 2, title: "Numerical Practice"),
+  ListViewUnit3(id: 3, title: "Question Bank"),
+  ListViewUnit3(id: 4, title: "Quiz"),
+];
+
+List funcList = [
+  QuizUnit3(),
+  QuizUnit3(),
+  QuizUnit3(),
+  QuizUnit3(),
+];
