8550c222e839b624429d2007e3fba0faadd5cdf4
RohanDoshi21 <rohan.doshi02@gmail.com>
2021-06-28 19:51:54 +0530

    Added 3 Calculators

---
+++ Rohan/27-06-2021 (2)/lib/Home_Page/home_page.dart
@@ -0,0 +1,312 @@
+//Written By Dinesh
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'ListviewItems.dart';
+// import 'class.dart';
+// import '../units/unit1/unit1.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key, required User user})
+      : _user = user,
+        super(key: key);
+
+  final User _user;
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  late User _user;
+  // bool _isSigningOut = false;
+
+  @override
+  void initState() {
+    _user = widget._user;
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Color(0xFF0a1931),
+      appBar: AppBar(
+        elevation: 20,
+        shadowColor: Colors.black,
+        title: Text(
+          'ChemAssist',
+          style: TextStyle(fontSize: 25),
+        ),
+        backgroundColor: Color(0xFF0a1931),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: size.height * .24,
+            width: double.infinity,
+            // color: Colors.grey,
+            child: Stack(
+              alignment: Alignment.bottomRight,
+              children: [
+                Positioned(
+                  top: 38,
+                  left: 20,
+                  child: Container(
+                    // color: Colors.black,
+                    margin: EdgeInsets.all(8),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Welcome, ' + _user.displayName!,
+                        style:
+                            TextStyle(color: Color(0xFFfdfaf6), fontSize: 25),
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 93,
+                  left: 20,
+                  child: Container(
+                    // color: Colors.black,
+                    margin: EdgeInsets.all(8),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Wrap(direction: Axis.horizontal, children: [
+                        //Stackoverflow
+                        RichText(
+                          textAlign: TextAlign.left,
+                          softWrap: true,
+                          text: TextSpan(children: <TextSpan>[
+                            TextSpan(
+                                text:
+                                    "Get the Unitwise study material & quizzes ",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 17)),
+                          ]),
+                        ),
+                      ]),
+                    ),
+                  ),
+                ),
+                //Three dots
+                Positioned(
+                  top: 18,
+                  left: 35,
+                  child: Container(
+                    height: 15,
+                    width: 15,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(80),
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 13,
+                  left: 10,
+                  child: Container(
+                    height: 10,
+                    width: 10,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(80),
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 33,
+                  left: 12,
+                  child: Container(
+                    height: 20,
+                    width: 20,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(80),
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 18,
+                  right: 35,
+                  child: Container(
+                    height: 15,
+                    width: 15,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(80),
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 13,
+                  right: 10,
+                  child: Container(
+                    height: 10,
+                    width: 10,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(80),
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 33,
+                  right: 12,
+                  child: Container(
+                    height: 20,
+                    width: 20,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(80),
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+              child: Stack(children: [
+            Container(
+              margin: EdgeInsets.only(top: 70),
+              decoration: BoxDecoration(
+                  color: Color(0xFFf9f3f3),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40))),
+            ),
+            // MyList(),
+            ListView.builder(
+              itemBuilder: (context, index) => MyList(
+                itemIndex: index,
+                itemNum: itemsList[index],
+              ),
+              itemCount: itemsList.length,
+            )
+          ]))
+        ],
+      ),
+    );
+  }
+}
+
+class MyList extends StatelessWidget {
+  final int itemIndex;
+  final ListdisplayText itemNum;
+
+  MyList({required this.itemIndex, required this.itemNum});
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+      // color: Colors.blueAccent,
+      height: 160,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Container(
+            width: size.width - 45,
+            // width: double.infinity,
+            height: 145,
+            decoration: BoxDecoration(
+                // color: Colors.blueAccent,
+                borderRadius: BorderRadius.circular(40)),
+            child: GestureDetector(
+              onTap: () {
+                // print('tapped');
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => funcList[itemIndex]));
+              },
+              child: Container(
+                height: 145,
+                width: size.width - 30,
+                margin: EdgeInsets.only(bottom: 10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(40),
+                  boxShadow: [
+                    BoxShadow(
+                      // color: Color(0xFFff7b54),
+                      color: Colors.black,
+                      offset: const Offset(
+                        5.0,
+                        5.0,
+                      ),
+                      blurRadius: 3.0,
+                      spreadRadius: 0.5,
+                    ),
+                  ],
+                  color: Colors.white,
+                  gradient: new LinearGradient(
+                    colors: [
+                      Color(0xFF12c2e9),
+                      Color(0xFF21096e),
+                      // Color(0xFFf64f59),
+                      // Color(0xFF21094e),
+                    ],
+                    begin: FractionalOffset.centerLeft,
+                    end: FractionalOffset.centerRight,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: -25,
+            left: 0,
+            child: SizedBox(
+              height: 136,
+              width: size.width - 200,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: GestureDetector(
+                      child: Text(
+                        itemNum.title,
+                        style: TextStyle(fontSize: 25, color: Colors.white),
+                      ),
+                      onTap: () {
+                        // print('tapped');
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => funcList[itemIndex]),
+                        );
+                      },
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 15, left: 20),
+                      child: GestureDetector(
+                        child: Text(
+                          itemNum.chapter,
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
+                        onTap: () {
+                          // print('tapped');
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => funcList[itemIndex]),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ Rohan/27-06-2021 (2)/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
+//import 'home_page.dart';
+import './Home_Page/login_page.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
+      ),
+      home: Loginpage(),
+    );
+  }
+}
