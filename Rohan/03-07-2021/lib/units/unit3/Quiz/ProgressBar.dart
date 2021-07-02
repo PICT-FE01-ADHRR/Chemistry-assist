73b2474050ab0de1468a3e994f6d073547e4ba9e
Rohan Doshi <rohan.doshi02@gmail.com>
2021-07-03 00:22:11 +0530

    Quiz Unit 3 merged
    Quiz in unit 1 is not working

---
+++ Rohan/03-07-2021/lib/units/unit3/Quiz/ProgressBar.dart
@@ -0,0 +1,59 @@
+import 'package:Chmestry_Study_Assist/units/unit3/Quiz/controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ProgressBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 35,
+      margin: EdgeInsets.only(top: 12),
+      width: double.infinity,
+      decoration: new BoxDecoration(
+          // color: Colors.white,
+          border: Border.all(color: Color(0xFF3F4768)),
+          borderRadius: BorderRadius.circular(50)),
+      child: GetBuilder<QuestionController>(
+          init: QuestionController(),
+          builder: (controller) {
+            //print(controller.animation.value);
+            return Stack(
+              children: [
+                //Layout builder to give width to the progress bar.
+                LayoutBuilder(
+                  builder: (context, constraints) => Container(
+                    width: constraints.maxWidth * controller.animation.value,
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [Color(0xFF46A0AE), Color(0xFF00FFCB)],
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight,
+                        ),
+                        borderRadius: BorderRadius.circular(50)),
+                  ),
+                ),
+                Positioned.fill(
+                  top: 2,
+                  left: 5,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                            "${(controller.animation.value * 20).round()} sec"),
+                        //
+                        Container(
+                          alignment: Alignment.centerRight,
+                          child: Image.asset("assets/images/clock.jpg"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            );
+          }),
+    );
+  }
+}
